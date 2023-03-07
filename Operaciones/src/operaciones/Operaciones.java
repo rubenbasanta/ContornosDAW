0d1b0ec7fbfbecc27c31f7e8c5a7f0c76d4c605a
carlos-arranz <127198379+carlos-arranz@users.noreply.github.com>
2023-03-07 11:35:47 +0100

    Creado programa operaciones
    Creado programa operaciones con método suma(a,b)

---
+++ Operaciones/src/operaciones/Operaciones.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones;
+
+import static operaciones.operaciones.operaciones.suma;
+
+/**
+ *
+ * @author carlosab
+ */
+public class Operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resultado=suma(3,4);
+        System.out.println(resultado);
+    }
+    
+}
---
+++ Operaciones/src/operaciones/operaciones/operaciones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones.operaciones;
+
+/**
+ *
+ * @author carlosab
+ */
+public class operaciones {
+    public static int suma(int a, int b){
+    
+    return a+b;}
+    
+    public static double suma(double a, double b){
+    
+    return a+b;}
+}
