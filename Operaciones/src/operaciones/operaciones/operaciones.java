eff305d0d0c918e4ca0850ad327e0a6e09b4365a
rubenbasanta <127199602+rubenbasanta@users.noreply.github.com>
2023-03-14 10:33:53 +0100

    Update operaciones.java

--- Operaciones/src/operaciones/operaciones/operaciones.java
+++ Operaciones/src/operaciones/operaciones/operaciones.java
@@ -16,4 +16,12 @@ public class operaciones {
     public static double suma(double a, double b){
     
     return a+b;}
+    
+    public static int resta(int a, int b){
+    
+    return a-b;}
+    
+    public static double resta(double a, double b){
+    
+    return a-b;}
 }
